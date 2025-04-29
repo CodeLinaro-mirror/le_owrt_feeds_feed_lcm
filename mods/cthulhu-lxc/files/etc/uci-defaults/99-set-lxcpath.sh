7819c7c221ca1a40d3fea8cd46267d77f31d687d
Alin Nastac <alin.nastac@softathome.com>
2025-04-29 13:41:46 +0200

    Use $IPKG_INSTROOT path prefix on /etc/uci-defaults scripts that could be executed on host
    Problem:
    ========
    When overlayfs is not mounted /etc/uci-defaults scripts
    cannot modify other scripts.
    
    Cause:
    ======
    Writable partitions must not be allowed to contain executable code
    
    Solution:
    =========
    openwrt services can be modified at build time if paths
    are prefixed file paths with $IPKG_INSTROOT. When building the rootfs
    image, makefile will grep for this variable name in the /etc/uci-defaults
    scripts and when it is found it will:
     - run the script with $IPKG_INSTROOT set to the rootfs source dir
     - script gets removed from rootfs after successful execution
    
    Firstboot script also creates /etc/subuid and /etc/subgid symlinks
    to files located in /tmp.
    
    References: PCF-1106
    Signed-off-by: Alin Nastac <alin.nastac@softathome.com>

--- mods/cthulhu-lxc/files/etc/uci-defaults/99-set-lxcpath.sh
+++ mods/cthulhu-lxc/files/etc/uci-defaults/99-set-lxcpath.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-sed -i 's|\blxc.lxcpath[[:space:]]*=[[:space:]]*/srv/lxc\b|lxc.lxcpath = /etc/config/lxc|' /etc/lxc/lxc.conf
-egrep "\blxc.lxcpath[[:space:]]*=[[:space:]]*/etc/config/lxc\b" /etc/lxc/lxc.conf &&  mkdir -p /etc/config/lxc
+sed -i 's|\blxc.lxcpath[[:space:]]*=[[:space:]]*/srv/lxc\b|lxc.lxcpath = /etc/config/lxc|' "$IPKG_INSTROOT/etc/lxc/lxc.conf"
+egrep "\blxc.lxcpath[[:space:]]*=[[:space:]]*/etc/config/lxc\b" "$IPKG_INSTROOT/etc/lxc/lxc.conf" &&  mkdir -p "$IPKG_INSTROOT/etc/config/lxc"
+
+if [ -n "$IPKG_INSTROOT" ]; then
+        source "$IPKG_INSTROOT/lib/functions/make_writable.sh"
+
+        cd "$IPKG_INSTROOT" || exit 1
+
+        make_file_writable etc/subgid tmp
+        make_file_writable etc/subuid tmp
+fi
 
 exit 0
